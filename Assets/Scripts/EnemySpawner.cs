0d1cef22f81ea4cab6ffcb2cca255f434ea94c82
zlightningzap <wongdaokang@gmail.com>
2016-03-22 10:52:13 +0800

    - Updated buildings to have mesh collider - Enemy health updated with billboard effect for clarity - Enemy Spawner(WIP)

---
+++ Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    //Public variables
+    public GameObject Enemyprefab;
+    public GameObject canvas;
+    public float AmountToSpawn;
+    public float Interval;
+
+    //Private variable
+    private float Spawntimer = 0;
+    private int spawned = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Spawnenemies();
+    }
+
+    void Spawnenemies()
+    {
+        //Debug.Log("Triggered");
+
+        //Add delta time to the spawn timer
+        Spawntimer += Time.deltaTime;
+
+        //Check if spawntimer is more than the interval && the spawned amount is not more than the specified amount
+        if (Spawntimer >= Interval && spawned != AmountToSpawn)
+        {
+            //Create the enemy prefab and the current position
+            GameObject enemy = Instantiate(Enemyprefab, transform.position, Quaternion.identity) as GameObject;
+            //Set the parent of the gameobject to the world canvas
+            enemy.transform.SetParent(canvas.transform);
+            
+            Spawntimer = 0;
+            spawned += 1;
+        }
+    }
+}
