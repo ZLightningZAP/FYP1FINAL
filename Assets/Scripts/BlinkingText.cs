be0e310aa2c37bdcd606e149fe089abbb1187590
zlightningzap <wongdaokang@gmail.com>
2016-03-23 11:29:38 +0800

    - Blinking text v1.1

--- Assets/Scripts/BlinkingText.cs
+++ Assets/Scripts/BlinkingText.cs
@@ -6,10 +6,12 @@ using UnityEngine.EventSystems;
 public class BlinkingText : MonoBehaviour
 {
     public float transitionTime = 1;
+    public float waitTime = 2;
 
     Text text;
-    private bool gone;
+    private bool gone = false;
     private float transition;
+    private float wait;
 
     // Use this for initialization
     void Start()
@@ -26,13 +28,19 @@ public class BlinkingText : MonoBehaviour
     void TextTransitioning()
     {
         transition += Time.deltaTime;
-
+        
         if (gone == false && transition >= transitionTime)
         {
-            text.CrossFadeAlpha(0.0f, transitionTime, false);
-            gone = true;
-            transition = 0;
+            wait += Time.deltaTime;
+            if (wait >= waitTime)
+            {
+                text.CrossFadeAlpha(0.0f, transitionTime, false);
+                gone = true;
+                transition = 0;
+                wait = 0;
+            }
         }
+
         else if (gone == true && transition >= transitionTime)
         {
             text.CrossFadeAlpha(1.0f, transitionTime, false);
