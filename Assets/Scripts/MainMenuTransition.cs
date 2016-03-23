7ebe18f37b999c7911eca755b01d2f247d14434b
zlightningzap <wongdaokang@gmail.com>
2016-03-23 10:25:04 +0800

    - Main Menu can now slide from 1 screen to another

---
+++ Assets/Scripts/MainMenuTransition.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenuTransition : MonoBehaviour
+{
+    public GameObject MainMenuPanel;
+    public GameObject HighScorePanel;
+    public float TransitionTiming;
+
+    private Animator anim1;
+    private Animator anim2;
+    private bool MainMenu = true;
+
+    private float transitiontime = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        //Get component of the animator from the gameobject
+        anim1 = MainMenuPanel.GetComponent<Animator>();
+        anim2 = HighScorePanel.GetComponent<Animator>();
+
+        //Set their respective animation to false
+        anim1.enabled = false;
+        anim2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transitiontime += Time.deltaTime;
+
+        if(transitiontime >= TransitionTiming)
+        {
+            if(MainMenu == false)
+            {
+                MainMenuSlideIn();
+                transitiontime = 0;
+            }
+            else if(MainMenu == true)
+            {
+                HighScoreSlideIn();
+                transitiontime = 0;
+            }
+        }
+
+    }
+
+
+    void HighScoreSlideIn()
+    {
+        // Set their respective animation to true
+        anim1.enabled = true;
+        anim2.enabled = true;
+
+        anim1.Play("MainPanelSlideOut");
+        anim2.Play("HighscorePanelSlideIn");
+
+        MainMenu = false;
+    }
+
+
+    void MainMenuSlideIn()
+    {
+        // Set their respective animation to true
+        anim1.enabled = true;
+        anim2.enabled = true;
+
+        anim1.Play("MainPanelSlideIn");
+        anim2.Play("HighscorePanelSlideOut");
+
+        MainMenu = true;
+    }
+}
