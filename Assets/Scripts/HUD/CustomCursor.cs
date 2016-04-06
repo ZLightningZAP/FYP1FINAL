41095073adfbfeeb2f5018e03ce9747fbcaa0683
zlightningzap <wongdaokang@gmail.com>
2016-04-06 14:20:35 +0800

    - Custom cursor updated

---
+++ Assets/Scripts/HUD/CustomCursor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CustomCursor : MonoBehaviour
+{
+    //The texture that's going to replace the current cursor
+    public Texture2D cursorTexture;
+
+    //This variable flags whether the custom cursor is active or not
+    public bool ccEnabled = false;
+
+    private void Start()
+    {
+        SetCustomCursor();
+    }
+
+    private void OnDisable()
+    {
+        //Resets the cursor to the default
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    private void SetCustomCursor()
+    {
+        //Replace the 'cursorTexture' with the cursor
+        Cursor.SetCursor(this.cursorTexture, Vector2.zero, CursorMode.Auto);
+    }
+}
