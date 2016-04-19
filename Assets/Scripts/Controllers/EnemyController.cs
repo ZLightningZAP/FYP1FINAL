9df5ef53272a800e15e1dbddeabf27069f6a0414
zLabels <PrincetonCYK@hotmail.com>
2016-04-19 15:20:13 +0800

    Enemy Controller script (pool) , updates to VFX controller

---
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -0,0 +1,130 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class EnemyController : MonoBehaviour {
+
+    public static EnemyController current;
+
+    public int PoolSize;    //Starting pool size
+    public int GrowRate;    //Rate at which pool will grow when it is full
+
+    public GameObject EnemyMissile_Object; //Missile Object that will be pooled
+
+    private List<GameObject> EnemyMissile_Pool; //List containing all enemy missiles in the scene
+
+    //Every type of enemy available in game
+    enum ENEMY_TYPE
+    {
+        ENEMY_MISSILE,
+        MAX_ENEMY,
+    };
+
+	// Use this for initialization
+	void Start () {
+
+        InitializeMissilePool();
+	}
+
+    private void Awake()
+    {
+        current = this;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+    // Main function that will be called by other scripts to create VFX effects
+    public GameObject SpawnEnemy(Vector3 position, Quaternion rotation, ENEMY_TYPE type)
+    {
+        switch (type)
+        {
+            case ENEMY_TYPE.ENEMY_MISSILE:
+                return SpawnExplosionSpark_Type1(position, rotation);
+
+            default:
+                return null;
+        }
+    }
+
+    /*************************************************
+       Initialize Functions
+     *************************************************/
+    private void InitializeMissilePool()
+    {
+        //Only if object to be pooled is assigned
+        if (EnemyMissile_Object != null)
+        {
+            //If pool does not exist
+            if (EnemyMissile_Pool == null)
+            {
+                //Create new pool
+                EnemyMissile_Pool = new List<GameObject>();
+            }
+
+            //Creating objects in the pool based on size
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(EnemyMissile_Object);
+                obj.SetActive(false);
+                EnemyMissile_Pool.Add(obj);
+            }
+        }
+    }
+
+    /*************************************************
+    Spawning Functions
+    *************************************************/
+    private GameObject SpawnExplosionSpark_Type1(Vector3 position, Quaternion rotation)
+    {
+        //Check if pool exists
+        if (EnemyMissile_Pool == null)
+        {
+            //Getting the first non active game object in this pool
+            for (int i = 0; i < EnemyMissile_Pool.Count; i++)
+            {
+                //Incase this game object is null, we create one and return it
+                if (EnemyMissile_Pool[i] == null)
+                {
+                    GameObject obj = (GameObject)Instantiate(EnemyMissile_Object);
+
+                    obj.transform.position = position;
+                    obj.transform.rotation = rotation;
+                    obj.SetActive(true);
+                    EnemyMissile_Pool[i] = obj;
+                    return EnemyMissile_Pool[i];
+                }
+                //Reuse when game object is not active
+                if (!EnemyMissile_Pool[i].activeInHierarchy)
+                {
+                    EnemyMissile_Pool[i].transform.position = position;
+                    EnemyMissile_Pool[i].transform.rotation = rotation;
+                    EnemyMissile_Pool[i].SetActive(true);
+                    return EnemyMissile_Pool[i];
+                }
+            }
+
+            //Increase the size of the list based on the grow rate
+            for (int i = 0; i < GrowRate; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(EnemyMissile_Object);
+                obj.SetActive(false);
+                EnemyMissile_Pool.Add(obj);
+
+                //When we have finished adding the new elements
+                if (i == (GrowRate - 1))
+                {
+                    EnemyMissile_Pool[EnemyMissile_Pool.Count - 1].SetActive(true);
+                    return EnemyMissile_Pool[EnemyMissile_Pool.Count - 1];
+                }
+            }
+        }
+
+        //If it reaches here, grow rate is 0 and
+        //all elements in the list is already in use
+        // OR
+        //No game object is assigned thus pool is not created
+        return null;
+    }
+}
