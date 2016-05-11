b161a8a5cace15eaa35f8679b77aacaa081a1c14
zLabels <PrincetonCYK@hotmail.com>
2016-05-11 15:18:23 +0800

    new scene update

---
+++ Assets/Scripts/Controllers/FrameRateController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FrameRateController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void Awake()
+    {
+        Application.targetFrameRate = 60;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
