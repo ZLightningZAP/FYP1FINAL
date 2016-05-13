449350bfa40dc9953cbb16852a34712f043fc03a
zLabels <PrincetonCYK@hotmail.com>
2016-05-13 11:50:54 +0800

    added more objects to VFX pool

--- Assets/Scripts/Controllers/VFXController.cs
+++ Assets/Scripts/Controllers/VFXController.cs
@@ -19,6 +19,11 @@ public class VFXController : MonoBehaviour
 
     public GameObject DustCloud_Object; //Dust Cloud Effect that will be pooled
 
+    public GameObject Bullets_Object;  //Bullet effect that will be pooled
+    public GameObject BulletsLarge_Object;  //Large bullet effect that will be pooled
+    public GameObject MuzzleFlash_Object;   //Muzzle Flash effect that will be pooled
+    public GameObject MuzzleFlashRapid_Object;  //Muzzle flash rapid effect that will be pooled
+
     private List<GameObject> SparkType1_Pool;   //List containing all the type 1 Spark Effects in the scene
     private List<GameObject> SparkType2_Pool;   //List containing all the type 2 Spark Effects in the scene
 
@@ -30,6 +35,11 @@ public class VFXController : MonoBehaviour
 
     private List<GameObject> DustCloud_Pool;   //List containing all the Dust Cloud Effects in the scene
 
+    private List<GameObject> Bullets_Pool;   //List containing all the Bullets Effects in the scene
+    private List<GameObject> BulletsLarge_Pool;   //List containing all the Large Bullets Effects in the scene
+    private List<GameObject> MuzzleFlash_Pool;   //List containing all the Muzzle Flash Effects in the scene
+    private List<GameObject> MuzzleFlashRapid_Pool;   //List containing all the Rapid Muzzle Flash Effects in the scene
+
     //Types of VFX available in the scene
     public enum VFX_TYPE
     {
@@ -40,6 +50,10 @@ public class VFXController : MonoBehaviour
         EXPLOSION_TYPE1,
         EXPLOSIONSPARK_TYPE1,
         DUSTCLOUD,
+        BULLETS,
+        BULLETS_LARGE,
+        MUZZLEFLASH,
+        MUZZLEFLASH_RAPID,
         MAX_TYPE,
     };
 
@@ -64,6 +78,12 @@ public class VFXController : MonoBehaviour
         InitializeExplosionSpark_Type1();
 
         InitializeDustCloud();
+
+        //Firing Effects
+        InitializeBulletsEffect();
+        InitializeBulletsLargeEffect();
+        InitializeMuzzleFlash();
+        InitializeMuzzleFlashRapid();
     }
 
     // Main function that will be called by other scripts to create VFX effects
@@ -91,6 +111,18 @@ public class VFXController : MonoBehaviour
 
             case VFX_TYPE.DUSTCLOUD:
                 return SpawnDustCloud(position, rotation);
+
+            case VFX_TYPE.BULLETS:
+                return SpawnBulletsEffect(position, rotation);
+
+            case VFX_TYPE.BULLETS_LARGE:
+                return SpawnBulletsLargeEffect(position, rotation);
+
+            case VFX_TYPE.MUZZLEFLASH:
+                return SpawnMuzzleFlash(position, rotation);
+
+            case VFX_TYPE.MUZZLEFLASH_RAPID:
+                return SpawnMuzzleFlashRapid(position, rotation);
 
             default:
                 return null;
@@ -256,6 +288,94 @@ public class VFXController : MonoBehaviour
                 GameObject obj = (GameObject)Instantiate(DustCloud_Object);
                 obj.SetActive(false);
                 DustCloud_Pool.Add(obj);
+            }
+        }
+    }
+
+    private void InitializeBulletsEffect()
+    {
+        //Only if object to be pooled is assigned
+        if (Bullets_Object != null)
+        {
+            //If does not exist
+            if (Bullets_Pool == null)
+            {
+                //Create new pool
+                Bullets_Pool = new List<GameObject>();
+            }
+
+            //Creating objects in the pool based on size
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(Bullets_Object);
+                obj.SetActive(false);
+                Bullets_Pool.Add(obj);
+            }
+        }
+    }
+
+    private void InitializeBulletsLargeEffect()
+    {
+        //Only if object to be pooled is assigned
+        if (BulletsLarge_Object != null)
+        {
+            //If does not exist
+            if (BulletsLarge_Pool == null)
+            {
+                //Create new pool
+                BulletsLarge_Pool = new List<GameObject>();
+            }
+
+            //Creating objects in the pool based on size
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(BulletsLarge_Object);
+                obj.SetActive(false);
+                BulletsLarge_Pool.Add(obj);
+            }
+        }
+    }
+
+    private void InitializeMuzzleFlash()
+    {
+        //Only if object to be pooled is assigned
+        if (MuzzleFlash_Object != null)
+        {
+            //If does not exist
+            if (MuzzleFlash_Pool == null)
+            {
+                //Create new pool
+                MuzzleFlash_Pool = new List<GameObject>();
+            }
+
+            //Creating objects in the pool based on size
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(MuzzleFlash_Object);
+                obj.SetActive(false);
+                MuzzleFlash_Pool.Add(obj);
+            }
+        }
+    }
+
+    private void InitializeMuzzleFlashRapid()
+    {
+        //Only if object to be pooled is assigned
+        if (MuzzleFlashRapid_Object != null)
+        {
+            //If does not exist
+            if (MuzzleFlashRapid_Pool == null)
+            {
+                //Create new pool
+                MuzzleFlashRapid_Pool = new List<GameObject>();
+            }
+
+            //Creating objects in the pool based on size
+            for (int i = 0; i < PoolSize; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(MuzzleFlashRapid_Object);
+                obj.SetActive(false);
+                MuzzleFlashRapid_Pool.Add(obj);
             }
         }
     }
@@ -617,6 +737,218 @@ public class VFXController : MonoBehaviour
                     //Use the last object in the pool
                     DustCloud_Pool[DustCloud_Pool.Count - 1].SetActive(true);
                     return DustCloud_Pool[DustCloud_Pool.Count - 1];
+                }
+            }
+        }
+
+        //If it reaches here, grow rate is 0 and
+        //all elements in the list is already in use
+        // OR
+        //No game object is assigned thus pool is not created
+        return null;
+    }
+
+    private GameObject SpawnBulletsEffect(Vector3 position, Quaternion rotation)
+    {
+        //Check if pool exists
+        if (Bullets_Pool != null)
+        {
+            //Getting the first non active game object in this pool
+            for (int i = 0; i < Bullets_Pool.Count; i++)
+            {
+                //Incase this game object is null, we create one and return it
+                if (Bullets_Pool[i] == null)
+                {
+                    GameObject obj = (GameObject)Instantiate(Bullets_Object);
+
+                    obj.transform.position = position;
+                    obj.transform.rotation = rotation;
+                    obj.SetActive(true);
+                    Bullets_Pool[i] = obj;
+                    return Bullets_Pool[i];
+                }
+                //Reuse when game object is not active
+                if (!Bullets_Pool[i].activeInHierarchy)
+                {
+                    Bullets_Pool[i].transform.position = position;
+                    Bullets_Pool[i].transform.rotation = rotation;
+                    Bullets_Pool[i].SetActive(true);
+                    return Bullets_Pool[i];
+                }
+            }
+
+            //Increase the size of the list based on the grow rate
+            for (int i = 0; i < GrowRate; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(Bullets_Object);
+                obj.SetActive(false);
+                Bullets_Pool.Add(obj);
+
+                //When we have finished adding the new elements
+                if (i == (GrowRate - 1))
+                {
+                    //Use the last object in the pool
+                    Bullets_Pool[Bullets_Pool.Count - 1].SetActive(true);
+                    return Bullets_Pool[Bullets_Pool.Count - 1];
+                }
+            }
+        }
+
+        //If it reaches here, grow rate is 0 and
+        //all elements in the list is already in use
+        // OR
+        //No game object is assigned thus pool is not created
+        return null;
+    }
+
+    private GameObject SpawnBulletsLargeEffect(Vector3 position, Quaternion rotation)
+    {
+        //Check if pool exists
+        if (BulletsLarge_Pool != null)
+        {
+            //Getting the first non active game object in this pool
+            for (int i = 0; i < BulletsLarge_Pool.Count; i++)
+            {
+                //Incase this game object is null, we create one and return it
+                if (BulletsLarge_Pool[i] == null)
+                {
+                    GameObject obj = (GameObject)Instantiate(BulletsLarge_Object);
+
+                    obj.transform.position = position;
+                    obj.transform.rotation = rotation;
+                    obj.SetActive(true);
+                    BulletsLarge_Pool[i] = obj;
+                    return BulletsLarge_Pool[i];
+                }
+                //Reuse when game object is not active
+                if (!BulletsLarge_Pool[i].activeInHierarchy)
+                {
+                    BulletsLarge_Pool[i].transform.position = position;
+                    BulletsLarge_Pool[i].transform.rotation = rotation;
+                    BulletsLarge_Pool[i].SetActive(true);
+                    return BulletsLarge_Pool[i];
+                }
+            }
+
+            //Increase the size of the list based on the grow rate
+            for (int i = 0; i < GrowRate; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(BulletsLarge_Object);
+                obj.SetActive(false);
+                BulletsLarge_Pool.Add(obj);
+
+                //When we have finished adding the new elements
+                if (i == (GrowRate - 1))
+                {
+                    //Use the last object in the pool
+                    BulletsLarge_Pool[BulletsLarge_Pool.Count - 1].SetActive(true);
+                    return BulletsLarge_Pool[BulletsLarge_Pool.Count - 1];
+                }
+            }
+        }
+
+        //If it reaches here, grow rate is 0 and
+        //all elements in the list is already in use
+        // OR
+        //No game object is assigned thus pool is not created
+        return null;
+    }
+
+    private GameObject SpawnMuzzleFlash(Vector3 position, Quaternion rotation)
+    {
+        //Check if pool exists
+        if (MuzzleFlash_Pool != null)
+        {
+            //Getting the first non active game object in this pool
+            for (int i = 0; i < MuzzleFlash_Pool.Count; i++)
+            {
+                //Incase this game object is null, we create one and return it
+                if (MuzzleFlash_Pool[i] == null)
+                {
+                    GameObject obj = (GameObject)Instantiate(MuzzleFlash_Object);
+
+                    obj.transform.position = position;
+                    obj.transform.rotation = rotation;
+                    obj.SetActive(true);
+                    MuzzleFlash_Pool[i] = obj;
+                    return MuzzleFlash_Pool[i];
+                }
+                //Reuse when game object is not active
+                if (!MuzzleFlash_Pool[i].activeInHierarchy)
+                {
+                    MuzzleFlash_Pool[i].transform.position = position;
+                    MuzzleFlash_Pool[i].transform.rotation = rotation;
+                    MuzzleFlash_Pool[i].SetActive(true);
+                    return MuzzleFlash_Pool[i];
+                }
+            }
+
+            //Increase the size of the list based on the grow rate
+            for (int i = 0; i < GrowRate; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(MuzzleFlash_Object);
+                obj.SetActive(false);
+                MuzzleFlash_Pool.Add(obj);
+
+                //When we have finished adding the new elements
+                if (i == (GrowRate - 1))
+                {
+                    //Use the last object in the pool
+                    MuzzleFlash_Pool[MuzzleFlash_Pool.Count - 1].SetActive(true);
+                    return MuzzleFlash_Pool[MuzzleFlash_Pool.Count - 1];
+                }
+            }
+        }
+
+        //If it reaches here, grow rate is 0 and
+        //all elements in the list is already in use
+        // OR
+        //No game object is assigned thus pool is not created
+        return null;
+    }
+
+    private GameObject SpawnMuzzleFlashRapid(Vector3 position, Quaternion rotation)
+    {
+        //Check if pool exists
+        if (MuzzleFlashRapid_Pool != null)
+        {
+            //Getting the first non active game object in this pool
+            for (int i = 0; i < MuzzleFlashRapid_Pool.Count; i++)
+            {
+                //Incase this game object is null, we create one and return it
+                if (MuzzleFlashRapid_Pool[i] == null)
+                {
+                    GameObject obj = (GameObject)Instantiate(MuzzleFlashRapid_Object);
+
+                    obj.transform.position = position;
+                    obj.transform.rotation = rotation;
+                    obj.SetActive(true);
+                    MuzzleFlashRapid_Pool[i] = obj;
+                    return MuzzleFlashRapid_Pool[i];
+                }
+                //Reuse when game object is not active
+                if (!MuzzleFlashRapid_Pool[i].activeInHierarchy)
+                {
+                    MuzzleFlashRapid_Pool[i].transform.position = position;
+                    MuzzleFlashRapid_Pool[i].transform.rotation = rotation;
+                    MuzzleFlashRapid_Pool[i].SetActive(true);
+                    return MuzzleFlashRapid_Pool[i];
+                }
+            }
+
+            //Increase the size of the list based on the grow rate
+            for (int i = 0; i < GrowRate; i++)
+            {
+                GameObject obj = (GameObject)Instantiate(MuzzleFlashRapid_Object);
+                obj.SetActive(false);
+                MuzzleFlashRapid_Pool.Add(obj);
+
+                //When we have finished adding the new elements
+                if (i == (GrowRate - 1))
+                {
+                    //Use the last object in the pool
+                    MuzzleFlashRapid_Pool[MuzzleFlashRapid_Pool.Count - 1].SetActive(true);
+                    return MuzzleFlashRapid_Pool[MuzzleFlashRapid_Pool.Count - 1];
                 }
             }
         }
